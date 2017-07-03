77ce2613afe7579382d5f0f7f71f26d1a350f6e6
Ganesh Punalkar <ganesh.punalkar@gmail.com>
2017-07-03 22:17:21 +0530

    adding grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt){
+var gruntConfig = ({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    plato: {
+        acofCodeReport: {
+            files: {
+                'acof/report/': ['js/*.js']
+            }
+        }
+        },
+
+        yuidoc: {
+            compile: {
+                name: '<%= pkg.name %>',
+                description: '<%= pkg.description %>',
+                version: '<%= pkg.version %>',
+                url: '<%= pkg.homepage %>',
+                options: {
+                    paths: 'js',
+                    outdir: 'acof/docs/'
+                }
+            }
+        }
+    });
+
+grunt.initConfig(gruntConfig);
+grunt.loadNpmTasks('grunt-plato');
+grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
+grunt.registerTask('acof', function(target){
+        grunt.task.run([
+            'plato',
+            'yuidoc'
+        ])
+    });
+};
